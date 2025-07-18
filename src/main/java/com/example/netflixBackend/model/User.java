7fadc9819457126308bfef8d6b6b4c2d9f078a55
Onursal Toparlak <onursal.toparlak.5@gmail.com>
2025-07-18 15:29:26 +0300

    User Entity'si Oluşturuldu

---
+++ src/main/java/com/example/netflixBackend/model/User.java
@@ -0,0 +1,37 @@
+package com.example.netflixBackend.model;
+
+import lombok.*;
+import javax.persistence.*;
+import javax.validation.constraints.*;
+
+@Entity
+@Table(name = "users", uniqueConstraints = {
+    @UniqueConstraint(columnNames = "username"),
+    @UniqueConstraint(columnNames = "email")
+})
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @NotBlank
+    @Email
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+} 
