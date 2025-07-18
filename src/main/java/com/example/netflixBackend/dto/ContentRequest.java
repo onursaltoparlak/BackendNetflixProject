9d1e1df36ce499dd973e2af2a4fed1c5e8e80214
Onursal Toparlak <onursal.toparlak.5@gmail.com>
2025-07-18 15:43:58 +0300

    Content DTO’ları Oluşturuldu

---
+++ src/main/java/com/example/netflixBackend/dto/ContentRequest.java
@@ -0,0 +1,18 @@
+package com.example.netflixBackend.dto;
+
+import com.example.netflixBackend.model.ContentType;
+import lombok.Data;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Data
+public class ContentRequest {
+    private String title;
+    private ContentType type;
+    private LocalDate releaseDate;
+    private String genre;
+    private String castList;
+    private String director;
+    private String description;
+    private BigDecimal imdbRating;
+} 
